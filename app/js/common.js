63c6bac7a181a04a547140f784e7c4855deaa2a2
“Sergey <gulinweb@gmail.com>
2018-02-01 17:40:26 +0300

    carousel

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,33 @@
 $(function() {
 
-	// Custom JS
+	
+
+});
+
+$(document).ready(function(){
+  
+  
+
+  $('.car2').owlCarousel({
+    loop:true,
+    nav:true,
+    navText: ["<img src='../img/ic-ph/left.png'>","<img src='../img/ic-ph/right.png'>"],
+    dots:false,
+    autoplay:true,
+    smartSpeed:1000,
+    autoplayTimeout:2000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+  });
 
 });
